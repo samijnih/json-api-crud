618bac9ae6ba021af1e92ff018a883e8a2d342db
Sami Jnih <everytimeicob@gmail.com>
2020-03-03 23:10:28 +0100

    chore(user): Log every write actions.

--- src/Facade/UserFacade.php
+++ src/Facade/UserFacade.php
@@ -11,6 +11,7 @@ use App\Service\Clock;
 use App\User\EditUser;
 use App\User\RegisterUser;
 use App\User\RemoveUser;
+use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 
 final class UserFacade implements RegisterUser, EditUser, RemoveUser
@@ -18,26 +19,31 @@ final class UserFacade implements RegisterUser, EditUser, RemoveUser
     private UserRepository $entityRepository;
     private Clock $mutableClock;
     private Clock $immutableClock;
+    private LoggerInterface $domainLogger;
 
     public function __construct(
         UserRepository $entityRepository,
         Clock $mutableClock,
-        Clock $immutableClock
+        Clock $immutableClock,
+        LoggerInterface $domainLogger
     ) {
         $this->entityRepository = $entityRepository;
         $this->mutableClock = $mutableClock;
         $this->immutableClock = $immutableClock;
+        $this->domainLogger = $domainLogger;
     }
 
     public function register(string $firstName, string $lastName): void
     {
         $user = User::create(
-            $this->entityRepository->generateId(),
+            $id = $this->entityRepository->generateId(),
             new Identity($firstName, $lastName),
             $this->immutableClock
         );
 
         $this->entityRepository->create($user);
+
+        $this->domainLogger->info("User $id registered.");
     }
 
     public function edit(string $id, string $firstName, string $lastName): void
@@ -47,10 +53,14 @@ final class UserFacade implements RegisterUser, EditUser, RemoveUser
         $user->changeIdentity(new Identity($firstName, $lastName), $this->mutableClock);
 
         $this->entityRepository->update($user);
+
+        $this->domainLogger->info("User $id edited.");
     }
 
     public function remove(string $id): void
     {
         $this->entityRepository->delete(Uuid::fromString($id));
+
+        $this->domainLogger->info("User $id removed.");
     }
 }
